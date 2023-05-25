fe1c076f41779983c024449fa1741d7cd2bb9824
rafaeldpsilva <1190979@isep.ipp.pt>
2023-05-25 16:29:26 +0100

    new service

---
+++ src/services/LoginService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const LoginService = {
+  communityOperatorURL : 'http://192.168.2.171:5000',
+  community : "/community",
+  
+  async getCommunity() {
+    const path = this.communityOperatorURL+this.community
+    try{
+      const response = await axios.get(path);
+      console.log(response.data.community)
+      return response.data.community
+    }catch(error) {
+      console.error(error);
+    };
+  },
+};
+
+export default LoginService;
