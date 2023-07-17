03cdda333af53b7185c227a6a4f6b3cbda8bc110
rafaeldpsilva <1190979@isep.ipp.pt>
2023-07-17 15:46:29 +0100

    iots

--- src/views/components/TokenHistoricCard.vue
+++ src/views/components/TokenHistoricCard.vue
@@ -27,17 +27,17 @@
           </div>
         </li>
       </ul>
-      <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3">Expiring</h6>
-      <ul class="list-group">
-        <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg" v-for="token in expiringTokenList" :key="token.token">
+      <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3" v-if="this.expiringTokenList.length != 0" >Expiring</h6>
+      <ul class="list-group" v-if="this.expiringTokenList.length != 0">
+        <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg" v-for="token in expiringTokenList" :key="token.token">
           <div class="d-flex align-items-center">
             <argon-button
-              color="success"
+              color="danger"
               variant="outline"
               size="sm"
               class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
             >
-              <i class="fas fa-arrow-up" aria-hidden="true"></i>
+              <i class="fas fa-hourglass-end" aria-hidden="true"></i>
             </argon-button>
             <div class="d-flex flex-column">
               <h6 class="mb-1 text-dark text-sm">{{token.name}}</h6>
@@ -77,7 +77,7 @@ export default {
         var creationDate = new Date(this.tokenlist[i]['datetime']);
         const expireDate = new Date(creationDate.getTime() + this.tokenlist[i]['expiration_time_minutes'] * 60000)
         const diff = expireDate - new Date();
-        if(diff < 1000){
+        if(diff < 10080){
           this.expiringTokenList.push(this.tokenlist[i])
         }else{
           this.createdTokenList.push(this.tokenlist[i])
