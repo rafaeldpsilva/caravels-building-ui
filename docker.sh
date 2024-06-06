2c9b0e563d749beef10d2d2b089b0db5c6f087d8
rafaeldpsilva <1190979@isep.ipp.pt>
2024-06-06 17:40:04 +0100

    cc

--- docker.sh
+++ docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker rmi rdpds/tiocps-building-monitoring-ui:latest
-docker build -t rdpds/tiocps-building-monitoring-ui .
-id=$(docker images | grep 'rdpds/tiocps-building-monitoring*' | head -1 | awk '{print $3}')
-echo "ID: $id"
-docker tag $id rdpds/tiocps-building-monitoring-ui:latest
-docker push --all-tags rdpds/tiocps-building-monitoring-ui
+docker build -t rdpds/caravels-building-ui .
+docker tag rdpds/caravels-building-ui rdpds/caravels-building-ui:latest
+docker tag rdpds/caravels-building-ui rdpds/caravels-building-ui:v0.1.0
+docker push --all-tags rdpds/caravels-building-ui
